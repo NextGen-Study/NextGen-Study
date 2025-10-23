4862e04e8a2de22b49bcaf3da96378b6b76d9b20
Madhusudan Srivastava <madhavsri2702@gmail.com>
2025-10-23 18:25:50 +0530

    Responsive Navbar For Mobile

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-  // Select all main sections
+  function fadeAnimation() {
   const sections = document.querySelectorAll('.hero, .aboutUs, .services, .gallery, .enquiry, .feedback, .footer');
 
-  // Add fade-section class to all
+  
   sections.forEach(section => section.classList.add('fade-section'));
 
-  // Use Intersection Observer to trigger animation
   const observer = new IntersectionObserver((entries) => {
     entries.forEach(entry => {
       if (entry.isIntersecting) {
@@ -13,7 +12,28 @@
         entry.target.classList.remove('visible');
       }
     });
-  }, { threshold: 0.2 }); // 20% of section visible triggers fade
+  }, { threshold: 0.1 }); 
 
-  // Observe each section
   sections.forEach(section => observer.observe(section));
+  }
+
+  fadeAnimation();
+
+
+  function toggleMenu() {
+const menuBtn = document.querySelector('.menuButton');
+const menuLinks = document.querySelector('.menuLinks');
+const links = document.querySelectorAll('.menuLinks a');
+menuBtn.addEventListener('click', () => {
+   menuBtn.classList.toggle('active');
+  menuLinks.classList.toggle('active');
+});
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    menuLinks.classList.remove('active');
+    menuBtn.classList.remove('active');
+  });
+});
+  }
+
+  toggleMenu();
